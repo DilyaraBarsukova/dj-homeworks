ff573bdb95ae06595d480d18dce6f99f0e23110e
DilyaraBarsukova <122084490+DilyaraBarsukova@users.noreply.github.com>
2023-01-09 20:12:59 +0500

    Add files via upload

---
+++ first-project/first_project/app/migrations/views.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+from os import listdir
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.utils.timezone import now
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    # впишите правильные адреса страниц, используя
+    # функцию `reverse`
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': '',
+        'Показать содержимое рабочей директории': ''
+    }
+
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    return HttpResponse(os.listdir(path='.'))
